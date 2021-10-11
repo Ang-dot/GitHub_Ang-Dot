a6a507ece0d9baa7471c651528401925db964685
Ang Weoy Yang <weoyyang00@gmail.com>
2021-10-11 22:57:44 +0800

    Delete Python HackerRank Practice directory

--- Python HackerRank Practice/Loops.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    
-    # The provided code stub reads and integer, n, from STDIN. For all non-negative integers i < n, print i^2.
-    
-    n = int(input())
-
-    for i in range(0,n):
-        print(i ** 2)
